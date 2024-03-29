276f4c74f2c2ba9627746108fbe933888f3d9d2b
Eduardo Silva <eduardosilva.11@gmail.com>
2024-03-29 18:01:28 -0300

    Javascrip: Se sube toda la funcionalida del js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+//Botones de Editar y Agregar Cards
 const buttonEdit = document.querySelector(".profile__heading-edit");
 const buttonAdd = document.querySelector(".profile__heading-add");
+//Popup, formulario, perfil
 const popupProfile = document.querySelector(".popup_content_profile");
 const popupAddCard = document.querySelector(".popup_content_add-card");
 const formProfile = popupProfile.querySelector(".form");
@@ -10,6 +12,38 @@ const profileNodeTitle = document.querySelector(".profile__heading-title");
 const profileNodeSubtitle = document.querySelector(
   ".profile__heading-subtitle"
 );
+const newPlaceNameInput = formAddCard.querySelector(".form__input[name=title]");
+const newPlaceLinkInput = formAddCard.querySelector(".form__input[name=link]");
+const cardArea = document.querySelector(".cards__public");
+const popupImage = document.querySelector(".popup_image");
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+// Popup Edit Perfile....
 
 const popupCloseButtons = Array.from(
   document.querySelectorAll(".popup__close-btn")
@@ -21,15 +55,15 @@ function openPopup(popup) {
 function ClosePopup(popup) {
   popup.classList.remove("popup_open");
 }
-
+//Abrir Popup Profile
 buttonEdit.addEventListener("click", function () {
   openPopup(popupProfile);
 });
-
+//Abrir Popup AddCard
 buttonAdd.addEventListener("click", function () {
   openPopup(popupAddCard);
 });
-
+//Boton Cerrar Popups Abiertos
 popupCloseButtons.forEach((button) => {
   button.addEventListener("click", function () {
     const popup = button.closest(".popup");
@@ -42,4 +76,50 @@ formProfile.addEventListener("submit", function (event) {
   profileNodeTitle.textContent = inputProfileName.value;
   profileNodeSubtitle.textContent = inputProfileAbout.value;
   ClosePopup(popupProfile);
+});
+
+function addCard(event) {
+  event.preventDefault();
+  const cardNode = createCard(newPlaceNameInput.value, newPlaceLinkInput.value);
+
+  cardArea.prepend(cardNode);
+  popupAddCard.classList.remove("popup_open");
+}
+
+formAddCard.addEventListener("submit", addCard);
+
+function createCard(name, link) {
+  const template = document.querySelector(".template");
+  const templateNode = template.content.querySelector(".card");
+  const cardNode = templateNode.cloneNode(true);
+
+  cardNode.querySelector(".card__image-photo").src = link; //item.link;
+  cardNode.querySelector(".card__image-photo").alt = "Imagen de : " + name; //"Imagen de : " + item.name;
+  cardNode.querySelector(".card__content-title").textContent = name; //item.name;
+
+  //eliminar Card
+  cardNode
+    .querySelector(".card__image-delete")
+    .addEventListener("click", () => {
+      cardNode.remove();
+    });
+
+  const likeButton = cardNode.querySelector(".card__content-like");
+
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__content-like_Active");
+  });
+
+  cardNode.querySelector(".card__image-photo").addEventListener("click", () => {
+    popupImage.classList.add("popup_open");
+    popupImage.querySelector(".popup__image").src = link;
+    popupImage.querySelector(".popup__image").alt = "Imagen de : " + name;
+    popupImage.querySelector(".popup__image-title").textContent = name;
+  });
+  return cardNode;
+}
+initialCards.forEach((item) => {
+  const cardNode = createCard(item.name, item.link);
+
+  cardArea.append(cardNode);
 });
